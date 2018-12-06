4da2b2a8b89455f8bec5e94025f4aba6f77668af
Alejandro Meza <alejandro.meza@duke.edu>
2018-12-06 00:05:54 -0500

    gfds

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -85,7 +85,7 @@ public class HuffProcessor {
 		while (pq.size() > 1) {
 		    HuffNode left = pq.remove();
 		    HuffNode right = pq.remove();
-		    HuffNode t = new HuffNode(1,left.myWeight+right.myWeight,left, right);
+		    HuffNode t = new HuffNode(-1,left.myWeight+right.myWeight,left, right);
 		    pq.add(t);
 		}
 		
